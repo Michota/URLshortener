704946594252bf43ae1c840c75386959dde310bc
Michota <programistawindowsvista@gmail.com>
2022-09-04 19:08:45 +0200

    Styled and ready to used!

--- script.js
+++ script.js
@@ -1,59 +1,60 @@
-// select elements
+// select HTML elements
 const linkEl = document.getElementById("URLbox"),
   shortBtnEl = document.getElementById("shortButton"),
   shortenedLinkEl = document.getElementById("shortenedLink"),
   shortenedLink3El = document.getElementById("shortenedLink3"),
   originalLinkEl = document.getElementById("originalLink"),
   resultsEl = document.getElementById("results");
-// links
-let typedLink = undefined;
-let shortLink = undefined;
-let shortLink3 = undefined;
-let fullShortLink = undefined;
-let fullShortLink3 = undefined;
+
+// Variables for storing original and shortened links
+let originalLink = undefined,
+  shortenedLink = undefined,
+  shortenedLink3 = undefined,
+  fullshortenedLink = undefined,
+  fullshortenedLink3 = undefined;
 
 // button clicking
 shortBtnEl.addEventListener("click", function () {
-  typedLink = linkEl.value;
-  linkShortener(typedLink);
+  originalLink = linkEl.value;
+  linkShortenerAPI(originalLink);
 });
 
-const linkShortener = function (longLink) {
+const linkShortenerAPI = function (longLink) {
   fetch(`https://api.shrtco.de/v2/shorten?url=${longLink}`).then((res) => {
     if (res.ok) {
-      console.log("succes");
+      console.log("SUCCES - links were shortened!");
       res.json().then((data) => getLinks(data));
     } else {
-      console.log("fail");
+      console.log("Something went wrong...");
     }
   });
 };
 
+// Get links from API and save them in previosuly defined variables
 const getLinks = function (data) {
-  typedLink = data.result.original_link;
-  shortLink = data.result.short_link;
-  shortLink3 = data.result.short_link3;
-  fullShortLink = data.result.full_short_link;
-  fullShortLink3 = data.result.full_short_link3;
-  console.log(`Short Link: ${fullShortLink}, Original Link: ${typedLink}`);
+  originalLink = data.result.original_link;
+  shortenedLink = data.result.short_link;
+  shortenedLink3 = data.result.short_link3;
+  fullshortenedLink = data.result.full_short_link;
+  fullshortenedLink3 = data.result.full_short_link3;
+  // console.log(`Short Link: ${fullshortenedLink}, Original Link: ${originalLink}`);
   pageUpdate();
 };
 
+// Update visible links
 const pageUpdate = function () {
   resultsEl.classList.remove("hidden");
-  // change links
-  shortenedLinkEl.href = `${fullShortLink}`;
-  shortenedLink3El.href = `${fullShortLink3}`;
-  originalLinkEl.href = `${typedLink}`;
-  // change displayed links
+  // Change displayed text after generating short links
   shortenedLinkEl.textContent = `
-  ${shortLink}`;
+  ${shortenedLink}`;
   shortenedLink3El.textContent = `
-  ${shortLink3}`;
-  originalLinkEl.textContent = `${typedLink}`;
+  ${shortenedLink3}`;
+  originalLinkEl.textContent = `${originalLink}`;
 };
 
+// Copy link to clipboard
 const copyText = function (clickedElement) {
-  let copiedLink = clickedElement.href;
-  alert(copiedLink);
+  let copiedLink = clickedElement.textContent;
+  console.log(copiedLink);
+  navigator.clipboard.writeText(copiedLink);
 };
